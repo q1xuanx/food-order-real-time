31b07b9fa9dd80949ddaac686f6eefa118de91bd
nhan <nhoang2929@gmail.com>
2025-08-24 11:55:08 +0700

    add: base project

---
+++ src/main/java/ecomerece/food/order/controllers/HealthCheckController.java
@@ -0,0 +1,25 @@
+package ecomerece.food.order.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.Map;
+
+@RestController
+@Slf4j
+public class HealthCheckController {
+
+    // Api health check
+    @GetMapping("/health-check")
+    public Map<String, Object> healthCheck() {
+        log.info("Health check started");
+        Map<String, Object> response = Map.of("message", "SUCCESS", "status", true);
+        log.info("Health check finished with data: {} and time: {}", response, Date.from(Instant.now()));
+        return response;
+    }
+}
